345cab37291d5a500356977f3449e80a0f312d04
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-07 16:24:48 +0100

    Dadi Bonus: work in progress cicli annidati

--- dadi/bonus/js/script.js
+++ dadi/bonus/js/script.js
@@ -20,3 +20,21 @@ for (var i = 0; i < numeroPlayers; i++) {
 console.log(nomiPlayers);
 console.log(dadiPlayers);
 console.log(pareggioPlayers);
+while (pareggioPlayers.length!=1 && i==1) {
+  comparatore = 0;
+  for (i = 0; i < pareggioPlayers.length; i++) {
+    dado = Math.ceil(Math.random()*6);
+    dadiPlayers[pareggioPlayers[i]] = dado;
+    console.log('dado del giocatore che ha pareggiato ' + dadiPlayers[pareggioPlayers[i]] );
+    if (comparatore < dadiPlayers[i]) {
+      comparatore = dadiPlayers[i];
+      pareggioPlayers = [i];
+    } else if (comparatore == dadiPlayers[i]) {
+      pareggioPlayers.push(i);
+    }
+    console.log('al termine del ' + ++i + ' ciclo, c\'è/ci sono ' + pareggioPlayers.length + ' pareggio/i\n\n');
+    i--;
+    console.log(dadiPlayers);
+  }
+}
+console.log('A ' + nomiPlayers[pareggioPlayers[0]] + ' è uscito più grosso !');
