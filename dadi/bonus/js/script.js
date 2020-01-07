0fa9445c6a3204d94979bae7e31a2734cfc9789c
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-07 15:38:48 +0100

    Dadi Bonus: primi tentativi

--- dadi/bonus/js/script.js
+++ dadi/bonus/js/script.js
@@ -2,9 +2,20 @@ alert('Benvenuti a "Chi ce l\'ha più grosso", un gioco pensato per creare disag
 var numeroPlayers = parseInt(prompt('In quanti volete giocare?'));
 var nomiPlayers = [];
 var dadiPlayers = [];
+var pareggioPlayers = [];
+var comparatore = 0;
 for (var i = 0; i < numeroPlayers; i++) {
-  var nome = prompt('Nome Giocatore ' + i++ + ' : ');
+  var nome = prompt('Nome Giocatore ' + ++i + ' : ')
+  i--;
   nomiPlayers[i] = nome;
-  var dado = Math.ceil(Math.random()*10-4);
+  var dado = Math.ceil(Math.random()*6);
   dadiPlayers[i] = dado;
+  if (comparatore < dadiPlayers[i]) {
+    comparatore = dadiPlayers[i];
+    pareggioPlayers = [i];
+  }
+  if (comparatore == dadiPlayers[i]) {
+    pareggioPlayers.push(i);
+  }
 }
+for (i = 0; )
