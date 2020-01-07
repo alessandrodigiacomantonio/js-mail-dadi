1744d65ff12ac242008b0f1bc61f8ea704a41b31
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-07 17:40:31 +0100

    Dati Bonus: Ciclo infinito, non so perché :(

--- dadi/bonus/js/script.js
+++ dadi/bonus/js/script.js
@@ -20,19 +20,20 @@ for (var i = 0; i < numeroPlayers; i++) {
 console.log(nomiPlayers);
 console.log(dadiPlayers);
 console.log(pareggioPlayers);
-while (pareggioPlayers.length!=1 && i==1) {
+while (pareggioPlayers.length!=1) {
   comparatore = 0;
+  var pareggioPlayersCiclo = pareggioPlayers;
   for (i = 0; i < pareggioPlayers.length; i++) {
     dado = Math.ceil(Math.random()*6);
-    dadiPlayers[pareggioPlayers[i]] = dado;
-    console.log('dado del giocatore che ha pareggiato ' + dadiPlayers[pareggioPlayers[i]] );
-    if (comparatore < dadiPlayers[i]) {
-      comparatore = dadiPlayers[i];
-      pareggioPlayers = [i];
-    } else if (comparatore == dadiPlayers[i]) {
-      pareggioPlayers.push(i);
+    dadiPlayers[pareggioPlayersCiclo[i]] = dado;
+    console.log('rilancio dado del ' + ++i + ' giocatore che ha pareggiato: ' + dadiPlayers[pareggioPlayersCiclo[--i]] );
+    if (comparatore < dadiPlayers[pareggioPlayersCiclo[i]]) {
+      comparatore = dadiPlayers[pareggioPlayersCiclo[i]];
+      pareggioPlayers = pareggioPlayersCiclo[i];
+    } else if (comparatore == dadiPlayers[pareggioPlayersCiclo[i]]) {
+      pareggioPlayers.push(pareggioPlayersCiclo[i]);
     }
-    console.log('al termine del ' + ++i + ' ciclo, c\'è/ci sono ' + pareggioPlayers.length + ' pareggio/i\n\n');
+    console.log('al termine del ' + ++i + ' spareggio, c\'è/ci sono ' + pareggioPlayers.length + ' giocatori\n\n');
     i--;
     console.log(dadiPlayers);
   }
